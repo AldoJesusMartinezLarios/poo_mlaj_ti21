ae14e96b2a5970227be04700eef929e93c9359ec
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-01-24 19:58:14 +0000

    CREACION del programa2.py sobre formatear cadenas

---
+++ semana2/programa2.py
@@ -0,0 +1,21 @@
+"""
+    Programa 2
+    Nombre: Aldo Jesús Martínez Larios
+    Fecha: 24/01/2023
+    Descripción: Formatear cadenas que puedan ser string o enteras
+"""
+
+variable1 = "Hola " #variable de tipo str
+variable2 = 619 #variable de tipo int
+
+print("{}{}".format(variable1, variable2)) #formatea cadenas del tipo de datos que sean
+
+"""
+    El format nos permite formatear valores y no importa el tipo de dato que se este
+    utilizando. 
+    
+    AL UTULIZAR UN ESPACIO EN LAS LLAVES ESTE DA ERROR
+
+    LAS LLAVES SON LOS PARAMETROS QUE RECIBEN, ASI QUE LOS NUMEROS DE LLAVE DEPENDE SIEMPRE
+    DE LOS PARAMETROS
+"""

e00eeb68b2818481ef7e2b25c22d40c05827b99f
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-01-24 19:40:51 +0000

    MODIFICACION del programa1.py en replit

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 | No.1 | Archivo | Descripción |
 |------|---------|-------------|
 | 1    | program0.py| Hola mundo en python|
+| 2    | programa1.py| Comentarios multilinea, comentarios de una linea, concatenación y cadena de caracteres |
---
+++ semana2/programa1.py
@@ -0,0 +1,15 @@
+"""
+    Programa 1
+    Nombre: Aldo Jesús Martínez Larios
+    Fecha: 23/01/2023
+    Descripción: Comentarios multilinea, comentarios de una linea, concatenación y        cadena de                   caracteres
+"""
+variable1 = "Hola " # variable que almacena una cadena de caracteres
+variable2 = '619'  # variable que almacena un entero
+
+print (variable1 + variable2) #Concatena las cadenas
+
+"""
+    Comilla doble o comilla simple sirve para concatenar cadenas de caracteres
+    NO IMPORTA CUAL DE LAS DOS SE OCUPA
+"""

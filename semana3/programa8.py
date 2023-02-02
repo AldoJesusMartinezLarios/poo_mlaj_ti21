fe8b05d8fb017b6112eebf2373b7804e4f02f6f2
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-02-02 20:39:10 +0000

    ACTUALIZACION del programa8.py que compara dos números e imprime el mayor de ellos

--- semana3/programa8.py
+++ semana3/programa8.py
@@ -38,8 +38,8 @@ numero1 = int(input("Escribe un número: ")) #Pide un valor al usuario
 numero2 = int(input("Escriba otro número: ")) #Pide otro valor al usuario
 
 if numero1 == numero2: #Condicion
-    print("None") #Imprime un mensaje
+    print(None) #Imprime una cadena
 elif numero1 < numero2: #Condicion
-    print("Mayor: ",numero2) #Imprime un mensaje
+    print(numero2) #Imprime el numero mayor en esta condicion
 elif numero2 < numero1: #Condicion
-    print("Mayor: ",numero1) #Imprime un mensaje
+    print(numero1) #Imprime el numero mayor en esta condicion

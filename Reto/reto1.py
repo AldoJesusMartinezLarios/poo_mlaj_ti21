33e6aaf37edbb42b61ce1b1a0a042e7744eefd7b
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-02-04 03:40:21 +0000

    CREACION del reto1.py que registra la matricula y promedia cinco calificaciones

---
+++ Reto/reto1.py
@@ -0,0 +1,19 @@
+"""
+    Reto 1
+    Nombre: Aldo Jesús Martínez Larios
+    Fecha: 03/02/2023
+    Descripción: 
+"""
+
+matricula = int(input("Matricula: "))
+
+calificacion1 = float(input("Calificacion 1: "))
+calificacion2 = float(input("Calificacion 2: "))
+calificacion3 = float(input("Calificacion 3: "))
+calificacion4 = float(input("Calificacion 4: "))
+calificacion5 = float(input("Calificacion 5: "))
+
+promedio = (calificacion1+calificacion2+calificacion3+calificacion4+calificacion5)/5
+
+print("Matricula: ", matricula)
+print("Promedio: ", promedio)

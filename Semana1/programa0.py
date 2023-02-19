7dc69de6ebadb7158feb7d563839c061670a1804
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-02-19 00:50:12 +0000

    ACTUALIZACION de comentarios programa0.py

--- Semana1/programa0.py
+++ Semana1/programa0.py
@@ -1,2 +1,8 @@
+"""
+    Programa 0
+    Nombre: Aldo Jesús Martínez Larios
+    Fecha: 19/01/2023
+    Descripción: Hola mundo en python
+"""
 print ("Hola Mundo en python") # Imprime un mensaje en consola
 print ("Editado desde replit") #Imprime el mensaje en comillas en la consola
